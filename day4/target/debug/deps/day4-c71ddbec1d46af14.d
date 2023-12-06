595167bd2b71a0db7c889875460605f2f8758219
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2023-12-06 18:01:27 +0100

    day4

--- day4/target/debug/deps/day4-c71ddbec1d46af14.d
+++
@@ -1,5 +0,0 @@
-/home/grzesiek/documents/rust/advent-of-code-2023/day4/target/debug/deps/libday4-c71ddbec1d46af14.rmeta: src/main.rs
-
-/home/grzesiek/documents/rust/advent-of-code-2023/day4/target/debug/deps/day4-c71ddbec1d46af14.d: src/main.rs
-
-src/main.rs:

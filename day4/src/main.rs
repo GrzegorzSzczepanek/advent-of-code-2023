c7fd87525bf5f04cf288eb95a55b64ca2d4f0640
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2023-12-06 17:57:23 +0100

    day4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,31 +1,59 @@
 use std::fs::read_to_string;
 
+fn count_matching(card: &String) -> i32 {
+    let winning_and_mine: Vec<&str> = card.split(" | ").collect();
+    let winning: Vec<i32> = winning_and_mine[0]
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let mine: Vec<i32> = winning_and_mine[1]
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let mut count = 0;
+    for num in &mine {
+        if winning.contains(num) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
 fn part_1(cards: &Vec<String>) -> i32 {
     let mut result: i32 = 0;
 
     for card in cards {
-        let winning_and_mine: Vec<&str> = card.split(" | ").collect();
-        let winning: Vec<i32> = winning_and_mine[0]
-            .split_whitespace()
-            .map(|s| s.parse().unwrap())
-            .collect();
-        let mine: Vec<i32> = winning_and_mine[1]
-            .split_whitespace()
-            .map(|s| s.parse().unwrap())
-            .collect();
-
-        let mut count = 0;
-        for num in &mine {
-            if winning.contains(num) {
-                count += 1;
-            }
-        }
+        let count: i32 = count_matching(&card);
         if count != 0 {
-            result += i32::pow(2, count - 1);
+            result += i32::pow(2, (count - 1).try_into().unwrap());
         } else if count == 1 {
             result += 1;
         }
     }
+    return result;
+}
+
+fn part_2(cards: &Vec<String>) -> usize {
+    let mut result: usize = cards.len();
+    let mut copies: Vec<usize> = Vec::new();
+    for (i, card) in cards.iter().enumerate() {
+        let count: usize = count_matching(card) as usize;
+        for won_card_index in (i + 1)..(i + count + 1) {
+            copies.push(won_card_index);
+        }
+    }
+    let mut i = 0;
+    while i < copies.len() {
+        let copy = copies[i];
+        let count = count_matching(&cards[copy]) as usize;
+        for index in (copy + 1)..(copy + count + 1) {
+            copies.push(index);
+        }
+        i += 1;
+    }
+    result += copies.len();
     return result;
 }
 
@@ -40,8 +68,10 @@ fn main() {
             .replace("   ", " ")
             .replace("  ", " ")
             .replace(&str_to_replace, "");
-        println!("{card}");
+        // println!("{card}");
     }
     let part_1_result = part_1(&cards);
-    println!("{part_1_result}");
+    println!("Part 1 results: {part_1_result}");
+    let part_2_result = part_2(&cards);
+    println!("Part 2 results: {part_2_result}");
 }
